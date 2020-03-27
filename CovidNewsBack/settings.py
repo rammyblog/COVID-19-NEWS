c961e4d5f9c6b7f4087113d7576dcdc5eadcdeb9
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-03-27 20:35:38 +0100

    Made it only a get request

--- CovidNewsBack/settings.py
+++ CovidNewsBack/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xlstfmem+_d(vp$#h_n%z3)*elmdop(8**c#yq7s&nr)vaohh^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- news/views.py
+++ news/views.py
@@ -4,6 +4,6 @@ from rest_framework import generics
 from .serializers import NewsSerializers
 
 
-class NewsList(generics.ListCreateAPIView):
+class NewsList(generics.ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializers
