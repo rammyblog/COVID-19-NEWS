ebfc24d30ce99353fdd507a463265b022aa49fd4
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-03-27 21:47:51 +0100

    Made changes to the db

--- CovidNewsBack/settings.py
+++ CovidNewsBack/settings.py
@@ -75,13 +75,19 @@ WSGI_APPLICATION = 'CovidNewsBack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
