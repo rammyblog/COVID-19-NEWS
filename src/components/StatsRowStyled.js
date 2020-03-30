a2bf8b1890cd78d7124023d38b94041ca6c11a8c
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-03-30 21:10:27 +0100

    Center the card

--- src/components/StatsRowStyled.js
+++ src/components/StatsRowStyled.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export const StatsRowStyled = styled.div`
-        display: flex;
-        gap: 18px;
+        display: grid;
+        gap: 10px;
         margin-bottom: 34px;
         text-align: center;
         justify-content: center;
-        font-weight: 700
+        font-weight: 700;
+        grid-auto-flow: column
 `
--- src/components/StyledCards.js
+++ src/components/StyledCards.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const CardsRowStyled = styled.div `
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-    gap: 1rem;
+    justify-content: center;
+    align-items: center;
 `
