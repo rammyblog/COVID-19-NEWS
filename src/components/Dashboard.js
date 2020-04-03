9c3f1d54d6e2e17b08e696f63bdf958b71e9a223
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-04-03 01:36:36 +0100

    Added the android download link and google analytics tag

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Cards from './Cards';
 import StatsRow from './StatsRow';
 import Android from '../assets/android.png';
+import {newsContext} from "../context/NewsContext";
 
 function Dashboard() {
-
+    const {news} = useContext(newsContext);
     return (
         <div className='center-block'>
             <StatsRow/>
             <Cards/>
-            <div className='android-container'>
-                <img className='img-thumbnail' alt='Download our Android App' src={Android}/>
-            </div>
+            {
+                news ? <div className='android-container'>
+                <a href='https://linkshortly.herokuapp.com/l/pi8ly/' ><img className='img-thumbnail' alt='Download our Android App' src={Android}/> </a>
+            </div> : null
+            }
+
         </div>
 
     );
