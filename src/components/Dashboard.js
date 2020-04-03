1be9bcd8c06ee47b330ea15b588b2b43db030907
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-04-03 01:36:12 +0100

    Added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Cards from './Cards';
 import StatsRow from './StatsRow';
+import Android from '../assets/android.png';
 
 function Dashboard() {
 
     return (
         <div className='center-block'>
-
             <StatsRow/>
             <Cards/>
+            <div className='android-container'>
+                <img className='img-thumbnail' alt='Download our Android App' src={Android}/>
+            </div>
         </div>
 
     );
